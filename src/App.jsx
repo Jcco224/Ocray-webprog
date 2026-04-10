d8481b80ce13913e3721412e9051490ae3f2611b
@Jcco224 <ocrayjohncarlo4@gmail.com>
2026-04-10 18:06:01 +0800

    lab act 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
-import Layout from './components/Layout';
-import ArticlePage from './pages/ArticlePage';
-import HomePage from './pages/HomePage';
-import AboutPage from './pages/Aboutpage';
-
-const routes = [
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        path: '',
-        element: <HomePage />,
-      },
-      {
-        path: 'about',
-        element: <AboutPage />,
-      },
-      {
-        path: 'articles',
-        element: <ArticlePage />,
-      },
-    ],
-  },
-];
-
-const router = createBrowserRouter(routes);
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
 
 function App() {
-  return <RouterProvider router={router} />;
-}
+  const [count, setCount] = useState(0)
 
-export default App;
+  return (
+    
+     <div> className="App"
+      <header className="App-header">
+        <h1>Welcome to my React App!!</h1>
+        <p>
+          Name: <strong>John Carlo Ocray</strong> <br />
+          Email: <strong>ocrayjohncarlo4@gmail.com</strong>
+          Other Info: <strong>Handsome</strong>
+          <a href="https://github.com/Jcco224/Ocray-webprog">"github profile"</a>
+          </p>
+      </header>
+      
+     </div>
+    
+  )
+  }
+
+
+export default App
+
